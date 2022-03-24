940b6098845a80091df7b1cca54d2d81cd299bf3
川本 翔 K.K <kawamoto@secsoft.inc.local>
2022-03-24 11:12:28 +0900

    項番10対応

--- Osero/Osr001.vb
+++ Osero/Osr001.vb
@@ -88,28 +88,7 @@ Public Class Osr001
 
                 Btn_Start.Text = "中断する"
             Else
-                '中断ボタンをクリックした時の処理
-                Dim result As DialogResult = MessageBox.Show("盤面を保存して中断しますか？",
-                                             "質問",
-                                             MessageBoxButtons.YesNoCancel,
-                                             MessageBoxIcon.Exclamation,
-                                             MessageBoxDefaultButton.Button2)
-                If result = DialogResult.Yes Then
-                    '「はい」が選択された時 
-                    If game.GameBreak() Then
-                        Application.Exit()
-                    End If
-
-
-                ElseIf result = DialogResult.No Then
-                    '「いいえ」が選択された時 
-                    Btn_Start.Text = "ゲームスタート"
-                    Application.Exit()
-
-                ElseIf result = DialogResult.Cancel Then
-                    '「キャンセル」が選択された時 
-                    '何も処理を行わない
-                End If
+                Me.Close()
             End If
 
         Catch ex As Exception
@@ -178,10 +157,33 @@ Public Class Osr001
 
     'フォームを閉じるとき
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
-        If game.GameStatus <> 1 Then
-            Exit Sub
-        End If
+        Try
+            '中断ボタンをクリックした時の処理
+            Dim result As DialogResult = MessageBox.Show("盤面を保存して中断しますか？",
+                                         "質問",
+                                         MessageBoxButtons.YesNoCancel,
+                                         MessageBoxIcon.Exclamation,
+                                         MessageBoxDefaultButton.Button2)
+            If result = DialogResult.Yes Then
+                '「はい」が選択された時 
+                If game.GameBreak() Then
+                    'Application.Exit()
+                End If
 
+
+            ElseIf result = DialogResult.No Then
+                '「いいえ」が選択された時 
+                Btn_Start.Text = "ゲームスタート"
+                'Application.Exit()
+
+            ElseIf result = DialogResult.Cancel Then
+                '「キャンセル」が選択された時 
+                e.Cancel = True
+            End If
+        Catch ex As Exception
+        End Try
+
+        Exit Sub
 
         'If MessageBox.Show("途中だけど止める？", "確認", MessageBoxButtons.YesNo) = DialogResult.No Then
         '    e.Cancel = True
